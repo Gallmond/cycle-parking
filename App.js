89abe16571455456fc18bf85db4b08aa54f54655
gavin <gallmond@gmail.com>
2021-09-04 18:30:45 +0100

    working map and circle before cycleparking from file

---
+++ App.js
@@ -0,0 +1,188 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { useState, useRef } from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import MapView, { Circle } from "react-native-maps";
+
+// import { CycleParking } from "./cycleparking-tools/CycleParking";
+
+// const cycleParking = new CycleParking();
+
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const mapRef = useRef(null);
+
+  const barnes_roundabout_latlon = [51.470624, -0.255804];
+
+  const latitudeDeltaToMetres = ( latitudeDelta ) => {
+    // 0.00001° = 1.11 m
+    // 1degree is approx 69 miles
+    const miles = latitudeDelta * 69
+    const metres = miles / 0.00062137
+    return metres
+  }
+
+  // circle props
+  const [circleProps, setCircleProps] = useState({
+    visible: false,
+    center:{latitude:barnes_roundabout_latlon[0], longitude: barnes_roundabout_latlon[1]},
+    radius:100
+  })
+
+  // keep track of the region, we need the deltas
+  const [mapRegion, setRegion] = useState(null)
+  const onRegionChangeCompleteHandler = (e) => {
+    console.log('onRegionChangeCompleteHandler e', e)
+    // {"latitude": 51.472480300910206, "latitudeDelta": 0.010265127233381577, "longitude": -0.2561464346945286, "longitudeDelta": 0.007725097239017487}
+    setRegion(e)
+  }
+
+  const [mapCamera, setMapCamera] = useState({
+    center: {
+      latitude: barnes_roundabout_latlon[0],
+      longitude: barnes_roundabout_latlon[1],
+    },
+    pitch: 0,
+    heading: 0,
+    altitude: 5,
+    zoom: 16, // 0 - 20
+  });
+
+  const onPressHandler = async (e) => {
+    console.log('onPressHandler e.nativeEvent', e.nativeEvent)
+    // {"action": "press", "coordinate": {"latitude": 51.4741348880686, "longitude": -0.25690030306577677}, "position": {"x": 435, "y": 781}}
+
+    const tapped_lat = e.nativeEvent.coordinate.latitude
+    const tapped_lon = e.nativeEvent.coordinate.longitude
+
+    // draw circle
+    drawCircleToFitWidth(tapped_lat, tapped_lon)
+
+    // get cycle parking
+    
+
+  }
+
+  const drawCircleToFitWidth = (lat, lon) => {
+    const latitudeDelta = mapRegion.latitudeDelta;
+    const new_centre = {
+      latitude: lat,
+      longitude: lon,
+    };
+    const new_radius = Math.min(latitudeDeltaToMetres(latitudeDelta) / 5, 400);
+    setCircleProps(prevState => {
+      const newState = {
+        ...prevState,
+        visible: true,
+        center: new_centre,
+        radius: new_radius,
+      };
+      console.log('new circle state', newState);
+      return newState;
+    });
+  };
+
+
+  const getCurrentCameraPosition = () => {
+    return mapRef.current.getCamera();
+  }
+
+
+  return (
+    <View  style={styles.container}>
+      <MapView
+        ref={mapRef}
+        style={styles.map}
+        camera={mapCamera}
+        onPress={onPressHandler}
+        onRegionChangeComplete={onRegionChangeCompleteHandler}
+        // initialRegion={{
+        //   latitude: 37.78825,
+        //   longitude: -122.4324,
+        //   latitudeDelta: 0.0922,
+        //   longitudeDelta: 0.0421,
+        // }}
+      >
+        {circleProps.visible ? <Circle center={circleProps.center} radius={circleProps.radius} /> : null}
+      </MapView>
+      
+    </View>
+
+    // <SafeAreaView style={backgroundStyle}>
+    //   <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+    //   <ScrollView
+    //     contentInsetAdjustmentBehavior="automatic"
+    //     style={backgroundStyle}>
+    //     <Header />
+    //     <View
+    //       style={{
+    //         backgroundColor: isDarkMode ? Colors.black : Colors.white,
+    //       }}>
+    //       <Section title="Step One">
+    //         Edit <Text style={styles.highlight}>App.js</Text> to change this
+    //         screen and then come back to see your edits.
+    //       </Section>
+    //       <Section title="See Your Changes">
+    //         <ReloadInstructions />
+    //       </Section>
+    //       <Section title="Debug">
+    //         <DebugInstructions />
+    //       </Section>
+    //       <Section title="Learn More">
+    //         Read the docs to discover what to do next:
+    //       </Section>
+    //       <LearnMoreLinks />
+    //     </View>
+    //   </ScrollView>
+    // </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  highlight: {
+    fontSize: 14,
+    color: 'white'
+  }
+ });
+
+export default App;
---
+++ cycleparking-tools/CycleParking.js
@@ -0,0 +1,224 @@
+// import * as fs from 'fs'
+import * as FileSystem from 'expo-file-system';
+import {encode, decode, bounds, adjacent, neighbours} from './GeohashingVeness.js'
+import { geohashQueryBounds } from 'geofire-common'
+import bigJSON from './cycleparking.json'
+
+class CycleParking{
+
+  constructor( debug = false ){
+    this.debug = debug
+
+    /**
+     * file holding the data, use this if setData is not used
+     */
+    this.default_data_file = `./cycleparking.json`
+
+    /**
+     * the actual place data object
+     */
+    this.data = {}
+    this.data = bigJSON
+    this.geohashes_created = false
+
+    /**
+     * store of geohash keys keys
+     */
+    this.geohash_reference = {}
+
+    // init with default data. The geohash references are not built until this is done
+    this.getData()
+    this.addGeohashesToData()
+
+  }
+
+  /**
+   * set this.cyclepark_data
+   * @param {object} cyclepark_data 
+   * @returns 
+   */
+  setData = ( cyclepark_data ) => {
+    this.data = cyclepark_data
+    if(!this.geohashes_created){
+      this.addGeohashesToData()
+      this.geohashes_created = true  
+    }
+    return this
+  }
+
+
+  // getter
+  getData = () => {
+    const keys = Object.keys(this.data)
+    console.log(`getData(): this.data has ${keys.length} keys`)
+    console.log(`getData(): first key: ${keys[0]} last key: ${keys[keys.length-1]}`)
+    console.log(`getData(): this.data[ ${keys[0]} ]: ${JSON.stringify(this.data[ keys[0] ])}`)
+    if(Object.keys(this.data).length === 0){
+      const file_content = FileSystem.readAsStringAsync( this.default_data_file, {encoding: 'utf8'} )
+      // const file_content = fs.readFileSync( this.default_data_file, {encoding:'utf-8'} )
+      const file_object = JSON.parse( file_content )
+      if(!file_object) throw new Error(`Could not read content of default_data_file ${this.default_data_file}`)
+      this.setData( file_object )
+    }
+    return this.data;
+  }
+
+
+  /**
+   * get the object of one place
+   * @param {string} place_id 
+   * @returns {object|undefined} the place json for one place (or undefined)
+   */
+  getCycleParkById = ( place_id ) => {
+    this.getData()[ place_id ]
+  } 
+
+
+  /**
+   * resolves with an array of places
+   * @param {number} lat 
+   * @param {number} lon 
+   * @param {number} radius_in_metres 
+   * @returns {Promise} 
+   */
+  getCycleParksInRange = ( lat, lon, radius_in_metres ) => {
+    return new Promise((resolve,reject)=>{
+      
+      const places = []
+
+      // get bounds 
+      const bounds = geohashQueryBounds([lat, lon], radius_in_metres);
+      console.log('bounds', bounds);
+      // const bounds = geofire.geohashQueryBounds([lat, lon], radius_in_metres);
+
+      // get keys from the reference in this bound
+      const all_keys = []
+      for (let i = 0, l = bounds.length; i < l; i++) {
+        const [start, end] = bounds[i];
+        all_keys.push( ...this.getPlaceKeysInGeohashBounds( start, end ) ) 
+      }
+
+      // collect places
+      for(let i=0, l=all_keys.length; i<l; i++){
+        let place_id = all_keys[i];
+        let this_place = this.getCycleParkById( place_id )
+
+        // if for whatever reason this key does not return a place
+        if(!this_place) continue
+
+        // skip this place if it's not within radius_in_metres of the search point
+        const dist_from_centre = this.getDistBetweenTwoPoints([lat, lon] , [this_place.lat, this_place.lon])
+        if(dist_from_centre > radius_in_metres) continue
+
+        // add the key as the id, it's not stored in the data object
+        this_place['id'] = place_id
+        this_place['dist'] = dist_from_centre.toFixed(1)
+
+        // add it to the array
+        places.push( this_place )
+      }
+
+      resolve(places) 
+    })
+  }
+
+
+  /**
+   * return array of keys that are found in the geohash_reference between given bounds
+   * @param {string} start 
+   * @param {string} end 
+   * @returns 
+   */
+  getPlaceKeysInGeohashBounds = (start, end) => {
+    start = start.replace('~', '')
+    end = end.replace('~', '')
+    const start_4 = start.substr(0, 4)
+    
+    const found_keys = []
+    for (const top_level_key in this.geohash_reference) {
+      if (
+        top_level_key.indexOf(start_4) === 0
+      ) {
+        for (const second_level_key in this.geohash_reference[top_level_key]) {
+          if(
+            second_level_key >= start
+            && second_level_key <= end
+          ){
+            found_keys.push( ...this.geohash_reference[top_level_key][second_level_key] )
+          }
+        }
+      }
+    }
+
+    return found_keys
+  }
+
+
+  /**
+   * set the 'geohash' property of all the objects in this.data
+   * @returns {this}
+   */
+  addGeohashesToData = () => {
+    console.log('addGeohashesToData(): typeof this.getData()', typeof this.getData());
+    console.log('addGeohashesToData(): JSON.stringify(this.getData()).substring(0,100)', JSON.stringify(this.getData()).substring(0,100));
+    for (const place_id in this.getData()) {
+      if (Object.hasOwnProperty.call(this.getData(), place_id)) {
+        // 9 chars is accurate to about 4 metres ish (and is also as accurate as the TFL coords will get)
+        console.log('addGeohashesToData(): place_id', place_id);
+        console.log('addGeohashesToData(): this.getData()[ place_id ]', this.getData()[ place_id ]);
+        this.getData()[ place_id ][ 'geohash' ] = encode( this.getData()[ place_id ].lat, this.getData()[ place_id ].lon, 9 ) 
+        this.addGeoHashReference( this.getData()[ place_id ][ 'geohash' ], place_id )
+      }
+    }
+    return this
+  }
+
+
+   /**
+   * Add the given key to the geohash reference
+   * @param {string} geohash
+   * @param {string} key 
+   */
+  addGeoHashReference = ( geohash, key )=>{
+    let geohash_4 = geohash.substr(0, 4)
+    // create object for this level if we don't have it
+    if( this.geohash_reference[ geohash_4 ] === undefined ) this.geohash_reference[ geohash_4 ] = {}
+
+    // create array for full hash if we don't have it
+    if( this.geohash_reference[ geohash_4 ][ geohash ] === undefined ) this.geohash_reference[ geohash_4 ][ geohash ] = []
+
+    // add to this level if it's not already in there
+    if( this.geohash_reference[ geohash_4 ][ geohash ].indexOf( key ) === -1 ){
+      this.geohash_reference[ geohash_4 ][ geohash ].push( key ) 
+    }
+  }
+
+
+  /**
+   * get distance between two points on a sphere https://www.movable-type.co.uk/scripts/latlong.html
+   * 
+   * @param {array} latlon_1 [latitude , longitude]
+   * @param {array} latlon_2 [latitude , longitude]
+   * @returns {number} the distance in metres
+   */
+  getDistBetweenTwoPoints = (latlon_1, latlon_2) => {
+    const [lat1, lon1] = latlon_1
+    const [lat2, lon2] = latlon_2
+    const R = 6371e3 // radius of the earth in metres (give or take)
+    const lat1rads = lat1 * Math.PI/180 // φ, λ in radians
+    const lat2rads = lat2 * Math.PI/180
+    const latdeltarads = (lat2-lat1) * Math.PI/180
+    const londeltarads = (lon2-lon1) * Math.PI/180
+
+    const a = Math.sin(latdeltarads/2) * Math.sin(latdeltarads/2) +
+              Math.cos(lat1rads) * Math.cos(lat2rads) *
+              Math.sin(londeltarads/2) * Math.sin(londeltarads/2)
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+
+    return R * c // in metres
+  }
+
+
+}
+
+export { CycleParking }
