3de8d3a370edd1912335130dd7104ca69b2c2400
gavin <gallmond@gmail.com>
2021-10-30 17:56:21 +0100

    removed failed asyncstorage for large items

--- Components/GavMaterial/SettingsPage/SettingsPage.js
+++ Components/GavMaterial/SettingsPage/SettingsPage.js
@@ -1,107 +1,142 @@
-import React, { Component } from "react";
-import { 
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, {Component} from 'react';
+import {
   View,
   StyleSheet,
   Text,
   Button,
   Switch,
   Alert,
-  PermissionsAndroid
+  PermissionsAndroid,
 } from 'react-native';
-import themes from "../../../Theme";
-import userSettings from "../../../UserSettings";
+import themes from '../../../Theme';
+import userSettings from '../../../UserSettings';
+
 
 /**
  * props
  *  onBookmarksChanged
  */
-class SettingsPage extends Component{
-
-  constructor(props){
-    super(props)
+class SettingsPage extends Component {
+  constructor(props) {
+    super(props);
 
     this.state = {
       alwaysShowImagesSwitchState: false,
       alwaysShowImagesSwitchEnabled: false,
-    }
+    };
 
     // get current settings, enable switch when ready
-    userSettings.get('alwaysShowImages').then( alwaysShowImages => {
+    userSettings.get('alwaysShowImages').then(alwaysShowImages => {
       this.setState({
         ...this.state,
         alwaysShowImagesSwitchEnabled: true,
         alwaysShowImagesSwitchState: alwaysShowImages,
       });
-    })
+    });
 
-    this.onAlertConfirm.bind(this)
-
+    this.onAlertConfirm.bind(this);
   }
 
-  onAlertConfirm(){
-    userSettings.set('bookmarks', [])
+  onAlertConfirm() {
+    userSettings.set('bookmarks', []);
     Alert.alert('Bookmarks Cleared');
     typeof this.props.onBookmarksChanged === 'function'
       ? this.props.onBookmarksChanged()
-      : null
+      : null;
   }
 
-  async locationPermissionButtonPressed(){
-    console.log('SettingsPage.locationPermissionButtonPressed()')
-    PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION).then(status => {
-      console.log('PermissionsAndroid.RESULTS.GRANTED', status,  PermissionsAndroid.RESULTS.GRANTED);
-      userSettings.set('canAccessDeviceLocation', status === PermissionsAndroid.RESULTS.GRANTED)
+  async locationPermissionButtonPressed() {
+    console.log('SettingsPage.locationPermissionButtonPressed()');
+    PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    ).then(status => {
+      console.log(
+        'PermissionsAndroid.RESULTS.GRANTED',
+        status,
+        PermissionsAndroid.RESULTS.GRANTED,
+      );
+      userSettings.set(
+        'canAccessDeviceLocation',
+        status === PermissionsAndroid.RESULTS.GRANTED,
+      );
 
-      if(status === PermissionsAndroid.RESULTS.GRANTED){
-
+      if (status === PermissionsAndroid.RESULTS.GRANTED) {
         Alert.alert(
           'You have already given permission to access device location',
           `If you want to remove this, please do so in your device permission settings`,
           [{text: 'Ok'}],
         );
-
       }
-
-    })
+    });
   }
 
-  async clearBookmarksButtonPressed(){
-    console.log('SettingsPage.clearBookmarksButtonPressed()')
+  updateTFLDataButtonPressed() {
+    console.log('SettingsPage.updateTFLDataButtonPressed()');
 
-    const bookMarksArray = await userSettings.get('bookmarks')
+    const promptText = {
+      title: 'Update TFL Data',
+      desc: 'Are you sure you want to update TFL data? This will use a lot of data (approx 80mb) and add approx 6mb to local storage',
+    };
 
-    if(bookMarksArray.length < 1){
+    Alert.alert(promptText.title, promptText.desc, [
+      {
+        text: 'Update',
+        onPress: () => {
+          console.log('UPDATE DATA HERE');
+        },
+      },
+      {
+        text: 'Cancel',
+        onPress: () => {
+          console.log('do nothing');
+        },
+        style: 'cancel',
+      },
+    ]);
+
+    // use hardcoded data file by default
+    // test reading from file storage
+    // download and format file and put in file storage
+    // use this file as the main tfl data file if it exists, else use default
+  }
+
+  async clearBookmarksButtonPressed() {
+    console.log('SettingsPage.clearBookmarksButtonPressed()');
+
+    const bookMarksArray = await userSettings.get('bookmarks');
+
+    if (bookMarksArray.length < 1) {
       Alert.alert('You have no bookmarks');
       return;
     }
 
     Alert.alert(
-      "Clear Bookmarks?",
+      'Clear Bookmarks?',
       `Really delete ${bookMarksArray.length} bookmarks? This can't be undone.`,
       [
         {
-          text: "Clear",
-          onPress: this.onAlertConfirm.bind(this)
+          text: 'Clear',
+          onPress: this.onAlertConfirm.bind(this),
         },
         {
-          text: "Cancel",
-          onPress: () => console.log("Cancel Pressed"),
-          style: "cancel"
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
         },
-      ]
+      ],
     );
-
   }
 
-  alwaysShowImagesChanged(){
-    const newState = !this.state.alwaysShowImagesSwitchState
+  alwaysShowImagesChanged() {
+    const newState = !this.state.alwaysShowImagesSwitchState;
     // change the state of the on-page button
     this.setState({alwaysShowImagesSwitchState: newState});
     // save the user setting
-    userSettings.set('alwaysShowImages', newState)
+    userSettings.set('alwaysShowImages', newState);
   }
 
-  render(){
+  render() {
     return (
       <View
         style={{
@@ -152,8 +187,8 @@ class SettingsPage extends Component{
               width: 150,
             }}>
             <Button
-              onPress={()=>{
-                this.clearBookmarksButtonPressed()
+              onPress={() => {
+                this.clearBookmarksButtonPressed();
               }}
               title="Clear"
               color={themes.main.primary}
@@ -184,23 +219,28 @@ class SettingsPage extends Component{
 
           {this.state.alwaysShowImagesSwitchEnabled && (
             <View
-            style={{
-              width: 150,
-            }}>
+              style={{
+                width: 150,
+              }}>
               <Switch
-                trackColor={{ false: themes.main.secondaryVariant, true: themes.main.secondary }}
-                thumbColor={true ? "#f5dd4b" : "#f4f3f4"}
+                trackColor={{
+                  false: themes.main.secondaryVariant,
+                  true: themes.main.secondary,
+                }}
+                thumbColor={true ? '#f5dd4b' : '#f4f3f4'}
                 ios_backgroundColor="#3e3e3e"
-                onValueChange={()=>{
+                onValueChange={() => {
                   // change switch state
-                  this.alwaysShowImagesChanged()
-                  this.setState({alwaysShowImagesSwitchState: !this.state.alwaysShowImagesSwitchState});
+                  this.alwaysShowImagesChanged();
+                  this.setState({
+                    alwaysShowImagesSwitchState:
+                      !this.state.alwaysShowImagesSwitchState,
+                  });
                 }}
                 value={this.state.alwaysShowImagesSwitchState}
               />
             </View>
           )}
-          
         </View>
 
         {/* re-request location permission */}
@@ -227,8 +267,8 @@ class SettingsPage extends Component{
               width: 150,
             }}>
             <Button
-              onPress={()=>{
-                this.locationPermissionButtonPressed()
+              onPress={() => {
+                this.locationPermissionButtonPressed();
               }}
               title="Prompt"
               color={themes.main.primary}
@@ -237,12 +277,42 @@ class SettingsPage extends Component{
           </View>
         </View>
 
-
+        {/* update TFL data */}
+        <View
+          style={{
+            width: '100%',
+            height: 100,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            paddingHorizontal: 20,
+          }}>
+          <Text
+            style={{
+              color: themes.main.text.onSecondary,
+              fontSize: 17,
+              fontWeight: 'bold',
+              textAlignVertical: 'center',
+            }}>
+            Update TFL Data
+          </Text>
+          <View
+            style={{
+              width: 150,
+            }}>
+            <Button
+              onPress={() => {
+                this.updateTFLDataButtonPressed();
+              }}
+              title="Update"
+              color={themes.main.primary}
+              accessibilityLabel="Update TFL Data"
+            />
+          </View>
+        </View>
       </View>
     );
   }
-
 }
 
-
-export default SettingsPage
+export default SettingsPage;
--- UserSettings.js
+++ UserSettings.js
@@ -4,27 +4,37 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
  * 'bookmarks' key contains an array of cyclepark ids
  * 'alwaysShowImages' key contains true|false key
  */
-class UserSettings{
-
-  constructor(){
+class UserSettings {
+  constructor() {
     this.AsyncStorage = AsyncStorage;
+
+    /**
+     * if a value is larger than this many bytes, store it with the large methods
+     */
+    this.largeValueMinSize = 1000000;
   }
 
-  get( key ){
-    return new Promise((resolve,reject)=>{
-      if( typeof key !== 'string' ) throw new Error('Key must be a string')
-      this.AsyncStorage.getItem( key ).then( value => {
-        resolve(JSON.parse( value )) 
-      })
+  get(key) {
+    return new Promise((resolve, reject) => {
+      if (typeof key !== 'string') throw new Error('Key must be a string');
+      this.AsyncStorage.getItem(key).then(value => {
+        resolve(JSON.parse(value));
+      });
     });
   }
 
-  set( key, value ){
-    return this.AsyncStorage.setItem( key, JSON.stringify(value) )
+  /**
+   *
+   * @param {string} key key
+   * @param {mixed} value value
+   * @returns promise with an err, if any
+   */
+  set(key, value) {
+    return this.AsyncStorage.setItem(key, JSON.stringify(value));
   }
 
 }
 
-const userSettings = new UserSettings()
+const userSettings = new UserSettings();
 
-export default userSettings
+export default userSettings;
