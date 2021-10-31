7b8c9ef15537f5cc5c199c244a677d583eca76ab
gavin <gallmond@gmail.com>
2021-10-31 13:43:58 +0000

    started adding export helper

---
+++ GavClasses/ExportHelper.js
@@ -0,0 +1,28 @@
+import FileHelper from "./FileHelper"
+
+/**
+ * purpose of this class is to provide herlpers for downloading/accessing the
+ * raw TFL exports
+ */
+class ExportHelper{
+
+  constructor(){
+    this.fh = new FileHelper()
+  }
+
+  /**
+   * download the raw export from TFL
+   * optionally save it to a file
+   */
+  downloadFromTfl(fileName = null){
+
+
+
+  }
+
+
+
+
+}
+
+export default ExportHelper
